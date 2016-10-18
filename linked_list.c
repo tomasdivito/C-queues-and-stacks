f6dd47b13f8f5475ac22f38e5930882a57ee7bf0
Tomas Di Vito <tomasdv2@gmail.com>
2016-10-18 13:08:47 -0300

    added a linked list (wip)

---
+++ linked_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  struct Node *next;
+  int value;
+};
+
+struct Linked_List {
+  struct Node *first;
+};
+
+void add(struct Linked_List*, int, int);
+void delete(struct Linked_List*, int);
+
+int main() {
+  
+
+  return 0;
+}
+
+void add(struct Linked_List* list, int position, int value) {
+
+}
+
+void delete(struct Linked_List* list, int position) {
+
+}
